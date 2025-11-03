6fb39ef4ab2e36f4bbdde37d93844c9c4711b812
Valeria Salgado <vasalma0515@gmail.com>
2025-11-03 18:26:20 -0500

    Sincronizando y resolviendo conflictos

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import ui.login;
 public class Main {
 
     public static void main(String[] args) {
-        login ms = new login();
+        login ms = new login(); //Holiiiiiiiiiii
         ms.setVisible(true);
         ms.setLocationRelativeTo(null);
         DBConnection.getConnection();
