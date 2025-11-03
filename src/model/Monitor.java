0da168ab62307560ade71f195c050e5b0ba33639
Valeria Salgado <vasalma0515@gmail.com>
2025-11-03 18:45:17 -0500

    Se nos perdió to' el progreso

--- src/model/Monitor.java
+++ src/model/Monitor.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Monitor extends Usuario {
-    private String materiaAsignada;
+    private String materiaAsignada; // holiii
     private int horasSemanales;
 
     public Monitor() {
