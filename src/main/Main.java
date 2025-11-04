e2e0f615507a0b60cf0f29905d50e7aab1855078
Valeria Salgado <vasalma0515@gmail.com>
2025-11-03 19:16:07 -0500

    gas

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import ui.login;
 public class Main {
 
     public static void main(String[] args) {
-        login ms = new login(); //Hello jiji
+        login ms = new login(); //ola
         ms.setVisible(true);
         ms.setLocationRelativeTo(null);
         DBConnection.getConnection();
